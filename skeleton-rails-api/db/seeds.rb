25f60f8f07cf7d88a42fba0c19d37c3c63e4fe04
Andrew Vaughan <lilmenso101@gmail.com>
2015-02-16 11:41:57 -0800

    Revised seed data for more randomness

--- skeleton-rails-api/db/seeds.rb
+++ skeleton-rails-api/db/seeds.rb
@@ -2,7 +2,7 @@
 # random_age = rand(9..32)
 # random_host = rand(1..10)
 
-locations = ['1521 Howard St, San Francisco', "1197 8th St, San Francisco", "3601 Fillmore St, San Francisco"]
+locations = ['1521 Howard St, San Francisco', "1197 8th St, San Francisco", "3601 Fillmore St, San Francisco", "137 Carmel St, San Francisco", "1711 9th Ave, San Francisco"]
 
 
 FactoryGirl.define do
@@ -20,12 +20,12 @@ FactoryGirl.define do
 
 
   factory :event do
-    title Faker::Lorem.sentence
-    description Faker::Lorem.sentence
+    title {Faker::Lorem.sentence}
+    description {Faker::Lorem.sentence}
     location {locations.sample}
-    date_start Faker::Date.forward(6)
-    date_end Faker::Date.forward(7)
-    people_count Faker::Number.number(2)
+    date_start {Faker::Date.forward(6)}
+    date_end {Faker::Date.forward(7)}
+    people_count {Faker::Number.number(2)}
     category {["Sports", "Entertainment", "Social", "Misc", "Video Games", "Food", "Outdoors"].sample}
 
   end
